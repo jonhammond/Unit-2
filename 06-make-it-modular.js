173ce06bb6b570081e7a2ecf0354e7142e360c85
Jon Hammond <jonh1016@gmail.com>
2016-02-18 21:40:11 -0700

    Added function to module.js that will return files of specified extension

--- 06-make-it-modular.js
+++ 06-make-it-modular.js
@@ -1,6 +1,9 @@
-// var fs = require('fs');
-// var path = require('path');
-// var module = require('./06-module.js');
+var fs = require('fs');
+var path = require('path');
+var module = require('./06-module.js');
+
+
+
 // console.log(module.sayTest());
 // fs.readdir(process.argv[2], function callback(err, list) {
 //   var listOfFileNames = list;
--- 06-module.js
+++ 06-module.js
@@ -1,5 +1,23 @@
-// module.exports = {
-//   sayTest: function() {
-//     console.log('test');
-//   },
-// };
+module.exports = stuff;
+
+var fs = require('fs');
+var path = require('path');
+var stuff={};
+
+var dir = process.argv[2];
+var ext1 = process.argv[3];
+
+fs.readdir(dir, function(err,files) {
+  if (err) {
+    throw err;
+  }
+  for (var i=0; i<files.length; i++) {
+    var ext2 = path.extname(files[i]);
+    if (ext2 === ext1) {
+    // console.log(ext2);
+    console.log(files[i]);
+    }
+  }
+});
+
+module.exports = stuff;
